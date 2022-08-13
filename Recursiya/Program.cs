9f573e0b27a32c3ce13c28bb03c712e12c174410
Алексей Шевченко <alex18w@mail.ru>
2022-08-13 17:59:25 +0500

    Рекурсия

---
+++ Recursiya/Program.cs
@@ -0,0 +1,25 @@
+﻿//double Factorial(int n)
+//{
+    //if(n == 1) return 1;
+    //else return n * Factorial(n-1);
+//}
+//for (int i = 1; i < 40; i++)
+//{
+    //Console.WriteLine($"{i}! = {Factorial(i)}");
+//}
+
+            // Вычсление Фиббоначи
+//f(1) = 1
+//f(2) = 1
+//f(3) = f(n-1) + f(n-2)
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
+
